6e6c8c4421e9dd93bd4a161ab3cae1f0cacd8364
Najko Jahn <najko.jahn@gmail.com>
2020-11-14 18:12:43 +0100

    try cran

--- install.R
+++ install.R
@@ -1,2 +1,1 @@
-install.packages(c("dplyr", "ggplot2", "janeaustenr", "rmarkdown", "tidytext"),
-                 repos = 'http://mran.revolutionanalytics.com/snapshot/2020-11-14/')
+install.packages(c("dplyr", "ggplot2", "janeaustenr", "rmarkdown", "tidytext"))
